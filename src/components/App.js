2e7a1a79b384d643341a8707f1e79226a08f7c62
Alex Love <lazd53@gmail.com>
2020-05-22 10:17:33 -0700

    Allow flipping between request & result pages on App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './App.css';
 import RequestScreen from './RequestScreen';
+import ResultsScreen from './ResultsScreen';
 
 
-function App() {
-  return (
-    <div className="App">
-      <RequestScreen/>
-    </div>
-  );
+class App extends React.Component {
+  state={ switchPage: "system" }
+
+  handleSwitchPageButton = (page) => {
+    console.log(page)
+    this.state.switchPage !== page && this.setState({switchPage: page})
+
+  }
+
+  render(){
+    const {switchPage} = this.state;
+    return (
+      <div className="App">
+        <h1> Easy PV panel calculator</h1>
+        <div className="flip-pages">
+          <button
+            className= {switchPage ==="system" ? "flip-page-button selected": "flip-page-button"}
+            onClick = { ()=>this.handleSwitchPageButton("system")  }
+          >System</button>
+          <button
+            className={switchPage ==="results" ? "flip-page-button selected": "flip-page-button"}
+            onClick = { ()=>this.handleSwitchPageButton("results")  }
+          >Results</button>
+        </div>
+        {switchPage==="system" && <RequestScreen/>}
+        {switchPage==="results" && <ResultsScreen/>}
+      </div>
+    );
+  }
+
 }
 
 export default App;
