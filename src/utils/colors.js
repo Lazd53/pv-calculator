6081c4f818521b24728fa9b8617695fdc74714d1
Alex Love <lazd53@gmail.com>
2020-05-26 16:49:34 -0700

    Setup colors Util file
    This will be the main location for color definitions for the different 
    results.

---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+export const resultsColors = [
+  ["#1261A0", "#e7eff5"], //blue
+  ["#2E8B57", "#d9f2e4" ], //green
+  ["#f5b700", "#fff3ce" ], //yellow
+  ['#ff4500', "#ffd9cc" ], //orange red
+  ['#dd74ff', "#f5d6ff" ] //purple
+]
